8edb5b5f1cda812906e47f9ab3cc19d97676c559
Celso David <celunico43@gmail.com>
2021-10-26 20:37:39 -0300

    rotina de cadastrar um novo item no banco

--- app/Http/Controllers/EquipamentosController.php
+++ app/Http/Controllers/EquipamentosController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Equipamento;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class EquipamentosController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Factory|Application|View
      */
     public function index()
     {
@@ -21,29 +26,38 @@ class EquipamentosController extends Controller
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Factory|Application|View
      */
     public function create()
     {
-        //
+        return view('equipamentos.create', ['action' => route('equipamento.store'), 'method' => 'post']);
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  Request  $request
+     * @return RedirectResponse
      */
-    public function store(Request $request)
+    public function store(Request $request): RedirectResponse
     {
-        //
+        $url = $request->get('redirect_to', route('equipamento.index'));
+        if (!$request->has('cancel') ) {
+            $dados = $request->all();
+            Equipamento::create($dados);
+            $request->session()->flash('message', 'Equipamento cadastrado com sucesso');
+        } else {
+            $request->session()->flash('message', 'Operação cancelada pelo usuário');
+        }
+
+        return redirect()->to($url);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function show($id)
     {
@@ -54,7 +68,7 @@ class EquipamentosController extends Controller
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function edit($id)
     {
@@ -64,9 +78,9 @@ class EquipamentosController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function update(Request $request, $id)
     {
@@ -77,7 +91,7 @@ class EquipamentosController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function destroy($id)
     {
